b97f8779b135785221e900f1ffa5f3b3a6a0422a
Official0mega <supr3m30mega@gmail.com>
2026-01-09 18:54:26 +0100

    Day 4 Completed

--- Day_004/code.py
+++ Day_004/code.py
@@ -4,3 +4,44 @@ Author: James
 """
 
 # Write your Python code here
+# Day 4 - Introduction to OOP
+
+# Class and Object
+class Car:
+    pass
+
+my_car = Car()
+
+# __init__ constructor
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+person1 = Person("James", 25)
+
+# Instance vs Class Variables
+class Dog:
+    species = "Animal"  # class variable
+
+    def __init__(self, name):
+        self.name = name  # instance variable
+
+dog1 = Dog("Bingo")
+dog2 = Dog("Max")
+
+print(dog1.name)
+print(dog2.name)
+print(dog1.species)
+print(dog2.species)
+
+# Methods and self
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print("Hello, my name is", self.name)
+
+student1 = Student("Ayo")
+student1.greet()
--- Day_004/notes.md
+++ Day_004/notes.md
@@ -8,3 +8,26 @@
 
 ## Key Notes
 -
+# Day 4 – Introduction to OOP (Object-Oriented Programming)
+
+## What I Learned Today
+- What Object-Oriented Programming (OOP) is
+- Difference between Classes and Objects
+- How __init__() works
+- Instance variables vs Class variables
+- Methods and the use of self
+
+## Key Concepts
+- OOP models code like real life
+- Class = blueprint
+- Object = real instance of a class
+- __init__() runs when an object is created
+- Instance variables belong to one object
+- Class variables are shared by all objects
+- Methods define what an object can do
+- self refers to the current object
+
+## Why OOP is Useful
+- Organizes code
+- Reuses logic
+- Makes large programs easier to manage
